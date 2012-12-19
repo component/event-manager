d888d789c23745ad2359fbcb5c652387e3fa4e62
TJ Holowaychuk <tj@vision-media.ca>
2012-12-19 07:09:47 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+# EventManager
+
+  Higher level event management designed to facilitate fluent
+  domain-specific event subscriptions.
+
+  For example [component/events](https://github.com/component/events)
+  uses `EventManager` to provide fluent dom node event subsciptions,
+  while [component/delegates](https://github.com/component/delegates)
+  does the same, however for delegated events.
+
+  You can think of this as the "Event Emitter" of the abstract event mechanism.
+
+## Installation
+
+    $ component install component/event-manager
+
+## API
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,137 @@
+
+
+/**
+ * Expose `EventManager`.
+ */
+
+module.exports = EventManager;
+
+/**
+ * Initialize an `EventManager` with the given
+ * `target` object which events will be bound to,
+ * and the `obj` which will receive method calls.
+ *
+ * @param {Object} target
+ * @param {Object} obj
+ * @api public
+ */
+
+function EventManager(target, obj) {
+  this.target = target;
+  this.obj = obj;
+  this._bindings = {};
+}
+
+/**
+ * Register bind function.
+ *
+ * @param {Function} fn
+ * @return {EventManager} self
+ * @api public
+ */
+
+EventManager.prototype.onbind = function(fn){
+  this._bind = fn;
+  return this;
+};
+
+/**
+ * Register unbind function.
+ *
+ * @param {Function} fn
+ * @return {EventManager} self
+ * @api public
+ */
+
+EventManager.prototype.onunbind = function(fn){
+  this._unbind = fn;
+  return this;
+};
+
+/**
+ * Bind to `event` with optional `method` name.
+ * When `method` is undefined it becomes `event`
+ * with the "on" prefix.
+ *
+ *    events.bind('login') // implies "onlogin"
+ *    events.bind('login', 'onLogin')
+ *
+ * @param {String} event
+ * @param {String} [method]
+ * @return {EventManager}
+ * @api public
+ */
+
+EventManager.prototype.bind = function(event, method){
+  var obj = this.obj;
+  var method = method || 'on' + event;
+  var args = [].slice.call(arguments, 2);
+
+  // method
+  var fn = obj[method];
+  if ('function' != typeof fn) {
+    throw new Error('failed to bind "' + event + '" to ' + method + '(), method does not exist');
+  }
+
+  // callback
+  function callback() {
+    var a = [].slice.call(arguments).concat(args);
+    fn.apply(obj, a);
+  }
+
+  // subscription
+  this._bindings[event] = this._bindings[event] || {};
+  this._bindings[event][method] = callback;
+
+  // bind
+  this._bind(event, callback);
+
+  return this;
+};
+
+/**
+ * Unbind a single binding, all bindings for `event`,
+ * or all bindings within the manager.
+ *
+ *     evennts.unbind('login', 'onLogin')
+ *     evennts.unbind('login')
+ *     evennts.unbind()
+ *
+ * @param {String} [event]
+ * @param {String} [method]
+ * @api public
+ */
+
+EventManager.prototype.unbind = function(event, method){
+  if (0 == arguments.length) return this.unbindAll();
+  if (1 == arguments.length) return this.unbindAllOf(event);
+  var fn = this._bindings[event][method];
+  this._unbind(event, fn);
+};
+
+/**
+ * Unbind all events.
+ *
+ * @api private
+ */
+
+EventManager.prototype.unbindAll = function(){
+  for (var event in this._bindings) {
+    this.unbindAllOf(event);
+  }
+};
+
+/**
+ * Unbind all events for `event`.
+ *
+ * @param {String} event
+ * @api private
+ */
+
+EventManager.prototype.unbindAllOf = function(event){
+  var bindings = this._bindings[event];
+  if (!bindings) return;
+  for (var method in bindings) {
+    this.unbind(event, method);
+  }
+};
